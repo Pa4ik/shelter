2b71e3deb50ad541eed8f0a3f88e2c59d97f82b3
Maxim Pachuzheuski <makspohp123@gmail.com>
2024-09-05 00:55:02 +0300

    feat:add slider and . refactor:modal

--- shelter/src/js/main.js
+++ shelter/src/js/main.js
@@ -55,12 +55,121 @@ function burgerSize() {
 window.addEventListener("resize", burgerSize);
 
 
+//slider
+const itemOne = '<div class="slider__item item item_1"> <img src="src/img/pets-jennifer.png" alt="pets Jennifer"><span class="slider__span">Jennifer</span><button class="slider__btn">Learn more</button></div>';
+const itemTwo = '<div class="slider__item item item_2"><img src="src/img/pets-Sophia.png" alt="pets Sophia"><span class="slider__span">Sophia</span><button class="slider__btn">Learn more</button></div>';
+const itemThree = '<div class="slider__item item item_3"><img src="src/img/pets-woody.png" alt="pets Woody"><span class="slider__span">Woody</span><button class="slider__btn">Learn more</button></div>';
+const itemFour = '<div class="slider__item item item_4"><img src="src/img/pets-scarlet.png" alt="pets Scarlett"><span class="slider__span">Scarlett</span><button class="slider__btn">Learn more</button></div>';
+const itemFive = '<div class="slider__item item item_5"><img src="src/img/pets-katrine.png" alt="pets Katrine"><span class="slider__span">Katrine</span><button class="slider__btn">Learn more</button></div>';
+const itemSix = '<div class="slider__item item item_6"><img src="src/img/pets-timmy.png" alt="pets Timmy"><span class="slider__span">Timmy</span><button class="slider__btn">Learn more</button></div>';
+const itemSeven = '<div class="slider__item item item_7"><img src="src/img/pets-Freddie.png" alt="pets Freddie"><span class="slider__span">Freddie</span><button class="slider__btn">Learn more</button></div>';
+const itemEight = '<div class="slider__item item item_8"><img src="src/img/pets-charly.png" alt="pets Charly"><span class="slider__span">Charly</span><button class="slider__btn">Learn more</button></div>';
+
+const arrPets = [itemOne, itemTwo, itemThree, itemFour, itemFive, itemSix, itemSeven, itemEight];
+
+const slider = document.querySelector(".slider");
+const leftSlider = document.querySelector(".left__slider");
+const rightSlider = document.querySelector(".right__slider");
+
+let prevStack = []; 
+let nextStack = []; 
+let currentItems = []; 
+
+// рандомные карточки 
+function randomSlide() {
+  const visibleSlides = visibleISlide();
+  const itemsSlide = arrPets.filter(item => !currentItems.includes(item));
+  const randomItems = [];
+  while (randomItems.length < visibleSlides) {
+    const randomIndex = Math.floor(Math.random() * itemsSlide.length);
+    const randomItem = itemsSlide.splice(randomIndex, 1)[0];
+    randomItems.push(randomItem);
+  }
+  return randomItems;
+}
+
+//рендер карточек
+function renderSlider(items) {
+  slider.innerHTML = ''; 
+  items.forEach(item => {
+    slider.innerHTML += item;
+  });
+  openModalClick()
+}
+
+
+function getSlider() {
+  currentItems = randomSlide();
+  for (let i = 0; i < 3; i++) { 
+    const prevItems = randomSlide();
+    prevStack.push(prevItems);
+  }
+  renderSlider(currentItems);
+  openModalClick(); 
+}
+getSlider();
+
+
+// очистка массивов
+function clearStacks() {
+  prevStack = [];
+  nextStack = [];
+}
+
+// обновление слайдера при изменении размера 
+function updateSlider() {
+  clearStacks();  
+  currentItems = randomSlide(); 
+  renderSlider(currentItems); 
+}
+
+// отображения карточек по разрешению
+function visibleISlide() {
+  const widthWindow = window.innerWidth;
+  if (widthWindow >= 1270) {
+    return 3; 
+  } else if (widthWindow >= 758) {
+    return 2;
+  } else {
+    return 1;
+  }
+}
+
+window.addEventListener('resize', updateSlider);
+
+
+
+// влево
+leftSlider.addEventListener('click', () => {
+  if (prevStack.length > 0) {
+    nextStack.push([...currentItems]); 
+    currentItems = prevStack.pop(); 
+    renderSlider(currentItems);
+  }else{
+    currentItems = randomSlide(); 
+  }
+  renderSlider(currentItems); 
+});
+
+
+// вправо
+rightSlider.addEventListener('click', () => {
+  prevStack.push([...currentItems]);
+  if (nextStack.length > 0) {
+    currentItems = nextStack.pop();
+  } else {
+    currentItems = randomSlide();  
+  }
+  renderSlider(currentItems);
+});
+
 
 //открытие и закрытие модалки 
 
+
 const sliderItem = document.querySelectorAll(".slider__item")
 const modalWindow = document.querySelector(".modal__window_pets")
-const closeModal =document.querySelector(".modal__closed")
+const closeModal = document.querySelector(".modal__closed")
 
 
 sliderItem.forEach(item => {
@@ -106,7 +215,7 @@ fetch('./src/js/pets.json')
     })
     .then(globalPets => {
       pets = globalPets;
-      console.log(pets);
+      // console.log(pets);
     });
 
 
@@ -129,6 +238,7 @@ const item7 = document?.querySelector(".item_7");
 const item8 = document?.querySelector(".item_8");
 
 
+//изменения данных в карточках
 function modalContent(index) { 
   imgModal.src = pets[index - 1].img;
   namePets.innerHTML = `${pets[index-1].name}`;
@@ -141,39 +251,33 @@ function modalContent(index) {
 }
 
 
-item1?.addEventListener("click", () => {
-  localStorage.setItem('item', 1); 
-  modalContent(1);
-  });
-  item2?.addEventListener("click", () => {
-  modalContent(2);
-  localStorage.setItem('item', 2);  
-  });
-  item3?.addEventListener("click", () => {
-    localStorage.setItem('item', 3); 
-  modalContent(3);
-  });
-  item4?.addEventListener("click", () => {
-    localStorage.setItem('item', 4); 
-  modalContent(4);
-  });
-  item5?.addEventListener("click", () => {
-    localStorage.setItem('item', 5); 
-  modalContent(5);
-  });
-  item6?.addEventListener("click", () => {
-    localStorage.setItem('item', 6); 
-  modalContent(6);
-  });
-  item7?.addEventListener("click", () => {
-    localStorage.setItem('item', 7);  
-  modalContent(7);
-  });
-  item8?.addEventListener("click", () => {
-    localStorage.setItem('item', 8); 
-  modalContent(8);
-  });
+//открытия модалок в слайдере
+function openModalClick() {
+  const sliderItems = document.querySelectorAll(".slider__item");
 
-
-  // test
-
+  sliderItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (item.classList.contains("item_1")) {
+        modalContent(1); 
+      } else if (item.classList.contains("item_2")) {
+        modalContent(2); 
+      } else if (item.classList.contains("item_3")) {
+        modalContent(3);
+      } else if (item.classList.contains("item_4")) {
+        modalContent(4);
+      } else if (item.classList.contains("item_5")) {
+        modalContent(5);
+      } else if (item.classList.contains("item_6")) {
+        modalContent(6);
+      } else if (item.classList.contains("item_7")) {
+        modalContent(7);
+      } else if (item.classList.contains("item_8")) {
+        modalContent(8);
+      }
+      else {
+      }
+      modalWindow.classList.add("modal_active");
+      document.documentElement.style.overflow = 'hidden';
+    });
+  });
+}
