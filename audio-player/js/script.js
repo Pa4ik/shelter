4fc437b9ed5dc9abae1c00cfd5a764313827da3b
Maxim Pachuzheuski <makspohp123@gmail.com>
2024-09-18 18:27:29 +0300

    feat:add function startSong

---
+++ audio-player/js/script.js
@@ -0,0 +1,31 @@
+const nameSong = document.querySelector('.song__name')
+const songExecutor = document.querySelector('.song__executor')
+const imgCover = document.getElementById('cover')
+const backImg = document.querySelector('.back__img')
+const audio = document.getElementById('audio')
+const player = document.querySelector('.main__container')
+const playBtn = document.querySelector('.play__pause')
+const nextBtn = document.getElementById('nextSong')
+const prevBtn = document.getElementById('prevSong')
+const durationTime = document.querySelector('.durationTime')
+const currentTimes = document.querySelector('.currentTime')
+const volumeInput = document.getElementById('volume__length');
+const songLength = document.getElementById('song__length')
+
+
+const songs = ['Don t Hurt Yourself', 'Don t Start Now' , 'Don t Speak' , 'Moi Lolita' , 'Incognito','Love Like Mine', 'MIDDLE OF THE NIGHT', 'Ceux qui rêvent' , 'Mental Funeral'];
+const artist = ['Beyonce', 'Dua Lipa' , 'No Doubt' , 'Alizée' , 'bludnymph' , 'Stela Cole', 'Elley Duhé', 'Pomme', 'Ehle ']
+
+let songIndex = 0;
+
+
+//начальная песня
+function startSong(song) {
+    backImg.src = `assets/img/cover${songIndex + 1}.png`
+    imgCover.src = `assets/img/cover${songIndex + 1}.png`
+    nameSong.innerHTML = song
+    songExecutor.innerHTML = artist[songIndex]
+    audio.src = `assets/audio/${song}.mp3`
+}
+startSong(songs[songIndex])
+
