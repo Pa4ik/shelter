05ac24a56f52c146359c1644e75648eb021434a1
Maxim Pachuzheuski <makspohp123@gmail.com>
2024-10-09 16:36:11 +0300

    feat:add render html

--- random-game/src/js/index.js
+++ random-game/src/js/index.js
@@ -36,3 +36,34 @@ const body = document.getElementById('body');
     },
 ]
 
+
+body.innerHTML = `
+    <header class="header">
+        <h1 class="h1">Hangman</h1>
+    </header>
+    <main class="main">
+        <div class="gallows__block">
+            <img class="gallows" src="./src/gallows-img/gallows-0.png">
+        </div>
+        <div class="keyboard__text">
+            <div class="input__span">
+                <p id="world__text">Слово:</p>
+                <Span id='quest__text'>Вопрос:</Span>
+                <p id="incorrect">Неверные буквы 0/6</p>
+            </div>
+            <div class="keyboard" id="keyboard">
+            </div>
+        </div>
+        <div class="modal-menu">
+            <div id="modals" class="modal-menu-info">
+               <div class="modal-menu-window">
+                    <h3 id="win__loss"></h3>
+                    <span id="word">Слово:</span>
+                    <button id="reset">Играть еще раз</button>   
+               </div>
+            </div>
+        </div> 
+    </main>
+   `
+
+   
